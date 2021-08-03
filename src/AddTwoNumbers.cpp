0d361ad811f683c31f8ae46c6161626383f4a506
liujianfengv <27178254@qq.com>
2021-08-03 22:57:48 +0800

    add 2 sln

---
+++ src/AddTwoNumbers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res = nullptr;
+        ListNode** cur = &res; 
+        int extra = 0;
+        while (l1 || l2 || extra) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+            extra = sum / 10;
+            *cur = new ListNode(sum % 10);
+            cur = &((*cur)->next);
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
+        }
+        return res;
+    }
+};
